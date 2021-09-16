7fc31f3afb0adcf3ba6a6eda6eede9a0eb99fddd
fabian <fabian@asistensi.com>
2021-09-16 14:17:47 -0400

    [IMP] alias structure

--- src/config/aliases.js
+++ src/config/aliases.js
@@ -1,9 +1,13 @@
-const aliases = (prefix = `src`) => ({
-  '@pages': `${prefix}/pages`,
-  '@components': `${prefix}/components`,
-  '@reducers': `${prefix}/reducers`,
-  '@utils': `${prefix}/utils`,
-  '@actions': `${prefix}/actions`,
+const path = require(`path`);
+const prefix =  path.join(path.resolve(__dirname, './src'))
+
+const aliases = () => ({
+  'pages': `${prefix}/pages`,
+  'components': `${prefix}/components`,
+  'reducers': `${prefix}/redux/reducers`,
+  'actions': `${prefix}/redux/actions`,
+  'utils': `${prefix}/utils`,
+  'styles': `${prefix}/styles`,
 });
 
 module.exports = aliases;
