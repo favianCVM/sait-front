13243845c1f4c24911698f520cf8ca3f393bc50b
fabian <fabian@asistensi.com>
2021-09-16 16:55:46 -0400

    [IMP] DOCKERIZE THE APP

--- src/components/common/index.js
+++
@@ -1,1 +0,0 @@
-import { Commons } from '@utils/index'
--- src/config/aliases.js
+++ src/config/aliases.js
@@ -4,6 +4,7 @@ const prefix =  path.join(path.resolve(__dirname, './src'))
 const aliases = () => ({
   'pages': `${prefix}/pages`,
   'components': `${prefix}/components`,
+  'containers': `${prefix}/containers`,
   'reducers': `${prefix}/redux/reducers`,
   'actions': `${prefix}/redux/actions`,
   'utils': `${prefix}/utils`,
